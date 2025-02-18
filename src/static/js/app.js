fb07fe5cfecfe47e74f2e45e89e95f5c155969e9
Phillip Yohner <phillip.n.yohner@disney.com>
2025-02-18 14:08:14 -0500

    Minor update for commit

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">You have no to-do items in your list yet! Let's add one above!</p>
+                <p className="text-center">You have no to-do items in your list yet! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
